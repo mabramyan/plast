afafbc29531a5acb4f4c6087c6e655200c5440e6
Mikhail <m.abramyan@fortunejack.com>
2020-09-19 02:00:11 +0400

    remove template link

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -82,9 +82,8 @@
             </div>
         </div>
         <div class="footer_copy_right">
-            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+            <p>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved 
         </div>
     </footer>
     <!-- footer_end -->
